d9a2b7ae95953a6976ae82b7b5563cbd4d7b9a23
Logan Williams <logan@firefly.llc>
2022-08-23 07:45:03 -0600

    Switched transition animation since fly is buggy on mobile screens.

--- src/lib/components/Transition.svelte
+++ src/lib/components/Transition.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
-	import { quintIn, quintOut } from 'svelte/easing';
+	import { fade } from 'svelte/transition';
+	import { quintIn } from 'svelte/easing';
 	const duration = 300;
 </script>
 
-<div
-	in:fly={{ delay: duration, duration: duration, x: 1200, opacity: 0.5, easing: quintOut }}
-	out:fly={{ duration: duration, x: -1200, opacity: 0.5, easing: quintIn }}
->
+<div in:fade={{ duration: duration, easing: quintIn }}>
 	<slot />
 </div>
